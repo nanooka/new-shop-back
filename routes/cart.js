812044cd7c4027d68c6abd1e7af866814a564a34
nanooka <102428722+nanooka@users.noreply.github.com>
2024-03-27 22:38:01 +0400

    separated remove and decrease quantity

--- routes/cart.js
+++ routes/cart.js
@@ -69,12 +69,12 @@ router.delete("/:id", async (req, res) => {
       return res.status(400).json({ message: "Product not found" });
     }
 
-    if (existingCartItem.quantity > 1) {
-      await db
-        .collection("cart")
-        .updateOne({ userId, id }, { $inc: { quantity: -1 } });
-      return res.status(200).json({ message: "Quantity updated successfully" });
-    }
+    // if (existingCartItem.quantity > 1) {
+    //   await db
+    //     .collection("cart")
+    //     .updateOne({ userId, id }, { $inc: { quantity: -1 } });
+    //   return res.status(200).json({ message: "Quantity updated successfully" });
+    // }
 
     const result = await db.collection("cart").deleteOne({ userId, id });
     res.status(200).json(result);
@@ -83,5 +83,83 @@ router.delete("/:id", async (req, res) => {
   }
   console.log(req.body);
 });
+
+router.patch("/:id", async (req, res) => {
+  let {
+    userId,
+    id,
+    image,
+    title,
+    price,
+    rating,
+    category,
+    description,
+    quantity,
+  } = req.body;
+  try {
+    const existingCartItem = await db
+      .collection("cart")
+      .findOne({ userId, id });
+
+    if (!existingCartItem) {
+      return res.status(400).json({ message: "Product not found" });
+    }
+
+    if (existingCartItem.quantity > 1) {
+      await db
+        .collection("cart")
+        .updateOne({ userId, id }, { $inc: { quantity: -1 } });
+      return res.status(200).json({ message: "Quantity updated successfully" });
+    }
+    if (existingCartItem.quantity == 1) {
+      const result = await db.collection("cart").deleteOne({ userId, id });
+      res.status(200).json(result);
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Could not remove product from favorites" });
+  }
+});
+
+// get user's cart product list
+router.post("/userCart", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    if (!userId) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+    const userCart = await db.collection("cart").find({ userId }).toArray();
+    res.status(200).json(userCart);
+  } catch (err) {
+    res.status(500).json({ error: "Could not get cart" });
+  }
+});
+
+// // get user's cart product by id
+// router.post("/userCart/:id", async (req, res) => {
+//   try {
+//     const userId = req.body.userId;
+//     const productId = req.params.id;
+//     console.log(userId, productId);
+
+//     if (!userId) {
+//       return res.status(400).json({ error: "User ID is required" });
+//     }
+//     if (!productId) {
+//       return res.status(400).json({ error: "Product ID is required" });
+//     }
+//     const cartItem = await db
+//       .collection("cart")
+//       .findOne({ userId, id: productId });
+//     console.log(cartItem);
+
+//     if (!cartItem) {
+//       return res.status(404).json({ message: "Product not found in cart" });
+//     }
+//     console.log(productId);
+//     res.status(200).json(cartItem);
+//   } catch (err) {
+//     res.status(500).json({ error: "Could not get cart" });
+//   }
+// });
 
 module.exports = router;
